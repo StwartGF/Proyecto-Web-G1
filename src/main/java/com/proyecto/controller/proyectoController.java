bbbf62b781028f76737329b864e60e0bbe5ef474
sgfof <sgfof@STWART08>
2023-07-02 21:11:21 -0600

    Armando la navegacion de servicios y empezando con el de importaciones

--- src/main/java/com/proyecto/controller/proyectoController.java
+++ src/main/java/com/proyecto/controller/proyectoController.java
@@ -1,13 +1,21 @@
 package com.proyecto.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class proyectoController {
     
     @GetMapping("/")
     public String Test (){
         return "principal";
     }
+    /*
+    @GetMapping("/servicios")
+    public String Test (){
+        return "importaciones";
+    }
+    */
 }
